78039d7d0a6277a8227e9efb46fc48cb3d13cba8
Laurent Nguyen <laurent.nguyen@cea.fr>
2022-04-05 09:22:59 +0200

    clean.sh: clean repository

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# rm -rf *~ */*~ \
+#    aclocal.m4 \
+#    autom4te.cache \
+#    depcomp \
+#    install-sh \
+#    missing \
+#    configure \
+#    config.h.in \
+#    Makefile.in \
+#    */Makefile.in
+
+git clean -f -x -d
+
+
+
